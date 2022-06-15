e3926247550d9b3cd36ad5cad1dc91798967ba09
principal Kelvo <kelvinnjoroowino@gmail.com>
2022-06-15 15:02:06 +0300

    Done.

--- main.js
+++ main.js
@@ -3,7 +3,52 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
+const modal = document.querySelector('#modal');
+const heart = document.querySelectorAll('.like-glyph')
 
+modal.classList.add('hidden');
+
+for(var i = 0; i < heart.length;i++){
+  heart[i].addEventListener('click',(e)=>{
+    let heartParent= e.target;
+    heartEvent(heartParent);
+    console.log(heartParent);
+})
+}
+function heartEvent(heartParent){
+  mimicServerCall().then(function(response){
+    heartClicked(heartParent);
+    console.log("Success!");
+    console.log(response);
+  }).catch(function(error){
+  modal.classList.remove('hidden');
+    console.log(error);
+  }
+  )
+}
+
+function heartClicked(heartParent) {
+  if(heartParent.innerHTML === EMPTY_HEART) {
+    //add class
+    heartParent.classList.add('activated-heart');
+    heartParent.innerHTML = FULL_HEART;
+    console.log("Success!");
+  }
+  else {
+    heartParent.classList.remove('activated-heart');
+    heartParent.innerHTML = EMPTY_HEART;
+  }
+}
+
+
+
+// let arrow = document.querySelectorAll('.arrow');
+// for(var i = 0; i < arrow.length;i++){
+//     arrow[i].addEventListener('click', (e)=>{
+//         let arrowParent= e.target.parentElement.parentElement;
+//         arrowParent.classList.toggle("showMenu")
+//     })
+// }
 
 
 
